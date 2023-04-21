e1783b19cddd5dba6e8ec12a6d6b0832c5b317ac
Sash <notsadako@yandex.ru>
2023-04-21 16:15:09 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/model/Person.java
@@ -0,0 +1,26 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+@Entity
+@Table(name = "person")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "age")
+    private int age;
+}
